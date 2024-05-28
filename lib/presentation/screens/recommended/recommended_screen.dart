0a320ab408b57f9b642de62deb10e1116470d263
Palejandrachavez <a19490311@itmexicali.edu.mx>
2024-05-27 20:10:15 -0700

    cambios presentation

--- lib/presentation/screens/recommended/recommended_screen.dart
+++ lib/presentation/screens/recommended/recommended_screen.dart
@@ -1,22 +1,6 @@
 import 'package:book/presentation/widgets/scrollviews/list_list.dart';
 import 'package:flutter/material.dart';
 
-class RecommendedScreen extends StatelessWidget {
-  final String name = "recomendacion";
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'flutter demo',
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
-      home: RecommendedScreen(),
-    );
-  }
-}
-
 class Recomendation extends StatefulWidget {
   const Recomendation({super.key});
   final String name = "recomendacion";
