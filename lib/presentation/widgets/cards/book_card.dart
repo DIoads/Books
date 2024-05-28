22f7861df0e2569e30a7a4073e0cc0af6b013b99
Palejandrachavez <a19490311@itmexicali.edu.mx>
2024-05-28 16:36:59 -0700

    diseño card

--- lib/presentation/widgets/cards/book_card.dart
+++ lib/presentation/widgets/cards/book_card.dart
@@ -12,10 +12,43 @@ class BookCard extends StatelessWidget {
     double width = screenSize.width;
 
     return Card(
-      child: SizedBox(
-        height: height / 5,
-        width: width / 2,
-        child: Center(child: Text(currentBook.getTitle)),
+      child: Card(
+        elevation: 30,
+        shadowColor: Colors.black,
+        color: Colors.greenAccent[100],
+        child: SizedBox(
+          height: height / 5,
+          width: width / 2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //  mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                currentBook.getTitle,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.green[900],
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Image(
+                  image: NetworkImage(currentBook.getBookImage),
+                  width: width / 3,
+                  height: height / 14),
+              SizedBox(
+                height: height / 20,
+                width: width / 4.5,
+                child: TextButton(
+                  onPressed: () {},
+                  //child: const Text('Enabled'),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green)),
+                  child: const Text('Detalles'),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
