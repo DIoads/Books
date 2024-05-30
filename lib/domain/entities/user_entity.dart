48818caf09ce6410b0935a16e43d26c8135580de
DIoads <jose.alejandro.guzman156@gmail.com>
2024-05-30 00:02:43 -0700

    userInfo

--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -1,1 +1,27 @@
+class UserEntity {
+  String age;
+  String? imageURL;
+  String username;
 
+  UserEntity({
+    required this.age,
+    this.imageURL,
+    required this.username,
+  });
+
+  String get getAge => age;
+  String? get gerImageUrl => imageURL;
+  String get getUsername => username;
+
+  set setAge(String age) {
+    this.age = age;
+  }
+
+  set setImageURL(String imageURL) {
+    this.imageURL = imageURL;
+  }
+
+  set setUsername(String username) {
+    this.username = username;
+  }
+}
